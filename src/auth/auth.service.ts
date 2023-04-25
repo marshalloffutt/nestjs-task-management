b1342ca3f55b46ac79023aa3bbdd4d0db91491f5
Marshall Offutt <marshalloffutt@gmail.com>
2023-04-25 10:03:36 -0500

    Adds bycrypt and implements password salt and hashing.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bycrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -17,13 +18,19 @@ export class AuthService {
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.usersRepository.create({ username, password });
+
+    const salt = await bycrypt.genSalt();
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    const user = this.usersRepository.create({
+      username,
+      password: hashedPassword,
+    });
 
     try {
       await this.usersRepository.save(user);
     } catch (error) {
       if (error.code === '23505') {
-        //duplicate username
         throw new ConflictException('Username already exists');
       } else {
         throw new InternalServerErrorException();
